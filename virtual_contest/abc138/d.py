45293766e9fde7c4a7b0361593c81a27ed7dff46
ryuki999 <ryuki999.1624@gmail.com>
2023-04-10 20:23:44 +0900

    update abc138 virtual-contest

--- virtual_contest/abc138/d.py
+++ virtual_contest/abc138/d.py
@@ -3,35 +3,60 @@ import sys
 # 再帰呼び出しの深さの上限を 120000 に設定
 sys.setrecursionlimit(10**6)
 
-def dfs(pos, visited, x):
-    """深さ優先探索を行う関数
-    posは現在位置、visited[x]は頂点xが青色かどうかを表す真偽値
-    """
-    visited[pos] = True
-    points[pos] += x
-    # print(points)
-    for i in G[pos]:
-        if visited[i] == False:
-            dfs(i, visited, x)
+
+def input():
+    return sys.stdin.readline()[:-1]
+
 
 N, Q = map(int, input().split())
-ab = [list(map(int, input().split())) for _ in range(N-1)]
-px = [list(map(int, input().split())) for _ in range(Q)]
 
-G = [list() for _ in range(N+1)]
+G = [list() for _ in range(N)]
+for _ in range(N - 1):
+    a, b = map(int, input().split())
+    G[a - 1].append(b - 1)
+    G[b - 1].append(a - 1)
 
-for a, b in ab:
-    G[a].append(b)
-    # G[b].append(a)
+# ab = [list(map(int, input().split())) for _ in range(N - 1)]
+# px = [list(map(int, input().split())) for _ in range(Q)]
+
+points = [0] * N
+for _ in range(Q):
+    p, x = map(int, input().split())
+    points[p - 1] += x
 
 # print(G)
 
+# visited = [False] * (N + 1)
 
-points = [0] * (N+1)
-for p, x in px:
-    # dfs(p, x)
-    visited = [False] * (N+1)
-    dfs(p, visited, x)
 
-print(*points[1:])
+# def dfs(pos, pre_pos, visited):
+#     """深さ優先探索を行う関数
+#     posは現在位置、visited[x]は頂点xが青色かどうかを表す真偽値
+#     """
+#     visited[pos] = True
+#     if pos != pre_pos:
+#         points[pos] += points[pre_pos]
+#     for i in G[pos]:
+#         if visited[i] == False:
+#             dfs(i, pos, visited)
 
+
+# dfs(1, 1, visited)
+
+
+def dfs(pos, pre_pos):
+    """深さ優先探索を行う関数
+    posは現在位置、visited[x]は頂点xが青色かどうかを表す真偽値
+    """
+    for i in G[pos]:
+        if i == pre_pos:
+            continue
+        points[i] += points[pos]
+        dfs(i, pos)
+
+
+dfs(0, 0)
+
+# print(points)
+
+print(*points)
