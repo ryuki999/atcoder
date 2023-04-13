f117e9d20de2e5f8596b57de37da9a5013547638
ryuki999 <ryuki999.1624@gmail.com>
2023-04-13 20:56:03 +0900

    update abc296

--- contest/abc296/d.py
+++ contest/abc296/d.py
@@ -1,14 +1,15 @@
+from math import ceil
+
 N, M = map(int, input().split())
 
-for num in range(M, N*N+1):
-    # t = []
-    for i in range(1, int(num**(1/2))+1):
-        j = num // i
-        if j > N:
-            continue
-        if num % i == 0 and i * j == num:
-            # print(f"{n} = {i} * {j}")
-            # t.append((i,j))
-            print(i*j)
-            exit()
-print(-1)
+ans = 10**26
+for i in range(1, ceil(M ** (1 / 2)) + 1):
+    b = ceil(M / i)
+    # print(i, b)
+    if b > N:
+        continue
+    if i <= N and b <= N:
+        ans = min(ans, b * i)
+if ans == 10**26:
+    ans = -1
+print(ans)
