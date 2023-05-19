8b66b78742a8c3d2ffd04e4cba462982ce99c59e
ryuki999 <ryuki999.1624@gmail.com>
2023-05-19 21:48:29 +0900

    update virtual-contest abc139

--- virtual_contest/abc139/c.py
+++ virtual_contest/abc139/c.py
@@ -1,20 +1,12 @@
-from bisect import bisect_left
-
 N = int(input())
 H = list(map(int, input().split()))
 
-LEN = 0
-L = []
-dp = [0] * (N + 1)
-
-for i in range(N):
-    pos = bisect_left(L, H[i])
-    dp[i] = pos
-
-    if dp[i] >= LEN:
-        LEN += 1
-        L.append(H[i])
+ans = 0
+c = 0
+for i in range(N - 1):
+    if H[i + 1] <= H[i]:
+        c += 1
     else:
-        L[dp[i]] = H[i]
-    print(pos, dp, L)
-print(LEN)
+        c = 0
+    ans = max(ans, c)
+print(ans)
